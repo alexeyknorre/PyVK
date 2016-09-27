65a2e0aacb070721c316d7b9c582d688e9b9ae13
Alexey Knorre <a.v.knorre@gmail.com>
2016-09-27 15:06:23 +0300

    stupid stuff with subdirs

--- code/__init__.py
+++ code/__init__.py
@@ -4,4 +4,14 @@ Created on Fri Aug 26 13:39:14 2016
 
 @author: Alexey
 """
+import os
+
+directory = '../results'
+
+# Create directory to save results
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+
 
